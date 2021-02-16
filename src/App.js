adf21e098e1efd4b01b184099f5859e8e58e3e06
Vlad <kvlad0610@gmail.com>
2021-02-16 23:26:13 +0200

    Добавил хедер

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import user from './user.json';
-import statistics from './statistical-data.json';
+import statisticalData from './statistical-data.json';
 import ProfileCard from './components/Profile/ProfileCard';
 import Statistics from './components/Statistics/Statistics';
+import Container from './components/Container/Container';
+import Header from './components/Heder/Heder';
 import './App.css';
 
 const App = () => {
   return (
     <>
-      <ProfileCard user={user} />
-      <Statistics title={'Upload stats'} statistics={statistics} />
+      <Header title={'Домашнее задание №1'} />
+      <Container>
+        <ProfileCard user={user} />
+        <Statistics title={'Upload stats'} statistics={statisticalData} />
+      </Container>
     </>
   );
 };
---
+++ src/components/Heder/Heder.jsx
@@ -0,0 +1,7 @@
+import styles from "./Heder.module.css";
+
+const Header = ({title}) => (
+  <header className={styles.header}><h1 className={styles.title}>{title}</h1></header>
+)
+
+export default Header
